5b9061117d62695623b4b793bcf9a11f70bb2af3
AkumuOmolo <akumu.omoloh@gmail.com>
2025-08-22 21:29:53 +0300

    Adds codegrade

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,21 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+       
+        def dfs(node):
+            if node is None:
+                return None
+  
+            if node.get("id") == id:
+                return node
+         
+            for child in node.get("children", []):
+                found = dfs(child)
+                if found:
+                    return found
+            return None
+
+      
+        return dfs(self.root)
